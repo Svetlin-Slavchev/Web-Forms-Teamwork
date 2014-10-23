961cc2d5ef415665f39db3d0aa326df9f59f924d
kalimar <kalimar@abv.bg>
2014-10-24 01:10:50 +0300

    removing unneeded logic from master page load

--- EBooks/EBooks.Web/Site.Master.cs
+++ EBooks/EBooks.Web/Site.Master.cs
@@ -68,10 +68,7 @@ namespace EBooks.Web
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if(!Roles.RoleExists("admin")){
-                Roles.CreateRole("admin");
-                Roles.AddUserToRole("Drago", "admin");
-            }
+
         }
 
         protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
