ebcda416a30ff9c7038b527e0c63fa238905eb32
Lyubomir Mitkov <lyubomir.mitkov@gmail.com>
2014-10-24 00:18:01 +0300

    default and master

--- EBooks/EBooks.Web/Default.aspx.cs
+++ EBooks/EBooks.Web/Default.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EBooks.Web.Factories;
+using EBooks.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,28 @@ namespace EBooks.Web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            BindData();
+        }
 
+        private void BindData()
+        {
+            // do not show edin and delet buttons if user is not in admin role
+            //if (!UserModel.IsAdmin(Page.User.Identity.Name))
+            //{
+            //    this.BooksGridView.Columns[2].Visible = false;
+            //    this.BooksGridView.Columns[3].Visible = false;
+            //}
+
+            List<BookModel> books = BookModelFactory.GetAll();
+
+            this.BooksGridView.DataSource = books;
+            this.BooksGridView.DataBind();
+        }
+
+        public void BooksGridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            this.BooksGridView.PageIndex = e.NewPageIndex;
+            this.BindData();
         }
     }
 }
